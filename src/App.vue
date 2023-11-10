a374c7bc69fdf5ff1316a2b20860658129897bd5
patrykcyran <83006094+patrykcyran@users.noreply.github.com>
2023-11-10 11:11:53 +0100

    final version

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
-<template src="./App.html"/>
+<template>
+  <div class="bg-primary text-white text-center m-2 p-3">
+    <h3>Produkt: {{ wielka(odwroc(nazwa))}}</h3>
+    <h3>Cena: {{ waluta(pobierzCalkowitaCene(nizszyPodatek), 3) }} PLN (niższa stawka)</h3>
+    <h3>Cena: {{ $filtry.walutaG(pobierzCalkowitaCene(wyzszyPodatek)) }} PLN (wyższa stawka)</h3>
+  </div>
+</template>
 
 <script>
 export default {
-  data: function () {
+  name: 'MojKomponent',
+  data: function() {
     return {
-      licznik: 0
+      nazwa: "Laptop",
+      cena: 1490,
+      nizszyPodatek: 12,
+      wyzszyPodatek: 23
     }
   },
   methods: {
-    przechwycKlikniecie() {
-      this.licznik++;
+    pobierzCalkowitaCene(podatek) {
+      return this.cena + (this.cena * (podatek / 100))
+    },
+    waluta(wartosc, miejsc) {
+      return new Intl.NumberFormat("pl-PL",
+          {style: "currency", currency: "PLN",
+          minimumFractionDigits: miejsc || 2,
+          maximumFractionDigits: miejsc || 2
+          }).format(wartosc);
+    },
+    odwroc(wartosc) {
+      return wartosc.split("").reverse().join("");
+    },
+    wielka(wartosc){
+      return wartosc[0].toUpperCase() + wartosc.slice(1);
     }
   },
-  computed: {
-    komunikat() {
-      return this.licznik == 0 ?
-          "Przycisk nie został kliknięty" : `Liczba kliknięć: ${this.licznik}`;
-    }
-  }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,15 @@ import {createApp} from "vue";
 import mojKomponent from "./App.vue"
 
 const app = createApp(mojKomponent);
+
+app.config.globalProperties.$filtry = {
+    walutaG(wartosc) {
+        return new Intl.NumberFormat("pl-PL",
+            {style: "currency", currency: "PLN",
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2
+            }).format(wartosc);
+    }
+}
+
 app.mount("#app");
