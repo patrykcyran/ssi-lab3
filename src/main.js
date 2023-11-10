a0fb4787c0b22faac491623ebc97bf0dcaccb124
patrykcyran <83006094+patrykcyran@users.noreply.github.com>
2023-11-10 10:07:03 +0100

    start with vue

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {createApp} from "vue";
+import mojKomponent from "./App.vue"
 
-let licznik = 1;
-let container = document.createElement("div");
-container.classList.add("text-center", "p-3");
-let msg = document.createElement("h1");
-msg.classList.add("bg-primary", "text-white", "p-3");
-msg.textContent = "Przycisk nie został kliknięty";
-let button = document.createElement("button");
-button.textContent = "Kliknij mnie";
-button.classList.add("btn", "btn-secondary");
-button.onclick = () => msg.textContent = `Przycisk kliknięty: ${licznik++}`;
-container.appendChild(msg);
-container.appendChild(button);
-let app = document.getElementById("app");
-app.parentElement.replaceChild(container, app);
+const app = createApp(mojKomponent);
+app.mount("#app");
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
+      'vue': 'vue/dist/vue.esm-bundler',
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
