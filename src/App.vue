16d00fb086b09dbdc72318cb2eb3601e7c353595
patrykcyran <83006094+patrykcyran@users.noreply.github.com>
2023-11-10 10:16:11 +0100

    vue with separate html file

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
+<template src="./App.html"/>
+
 <script>
 export default {
-  template: `
-    <div class="text-center p-3">
-    <h1 class="bg-secondary text-white p-3">
-      {{ komunikat }}
-    </h1>
-    <button class="btn btn-secondary" v-on:click="przechwycKlikniecie">
-      Kliknij mnie
-    </button>
-    </div>`,
   data: function () {
     return {
       licznik: 0
