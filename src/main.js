26fa75ba1f20ef78a1eaf92ac6c6d1cc4d4ad465
patrykcyran <83006094+patrykcyran@users.noreply.github.com>
2023-11-10 09:38:28 +0100

    pure js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import './assets/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+let licznik = 1;
+let container = document.createElement("div");
+container.classList.add("text-center", "p-3");
+let msg = document.createElement("h1");
+msg.classList.add("bg-primary", "text-white", "p-3");
+msg.textContent = "Przycisk nie został kliknięty";
+let button = document.createElement("button");
+button.textContent = "Kliknij mnie";
+button.classList.add("btn", "btn-secondary");
+button.onclick = () => msg.textContent = `Przycisk kliknięty: ${licznik++}`;
+container.appendChild(msg);
+container.appendChild(button);
+let app = document.getElementById("app");
+app.parentElement.replaceChild(container, app);
