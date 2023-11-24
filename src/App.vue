10e30f57f325c83707088eab978f03cc4dee301b
patrykcyran <83006094+patrykcyran@users.noreply.github.com>
2023-11-24 11:16:38 +0100

    lab - final

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div class="container-fluid text-center">
-    <div class="bg-primary text-white text-center m-2 p-3">
-      <ul class="text-left">
-        <li>Pierwszy element listy</li>
-        <template v-if="pokazElementy">
-          <li>{{nazwa}}</li>
-          <li>{{cena}}</li>
-        </template>
-        <li>Ostatni element listy</li>
-      </ul>
-      <h3 v-if="pokazElementy">Produkt: {{nazwa}}</h3>
-      <h3 v-else>Cena: {{cena}}</h3>
+    <h2 class="bg-primary text-white text-center p-3">Produkty</h2>
+    <table class="table table-sm table-bordered table-striped text-left">
+      <tr><th>Indeks (obiekt)</th><th>Klucz</th><th>Nazwa</th><th>Cena</th></tr>
+      <tbody>
+      <tr v-for="(p, key, i) in produktyObiekt" v-bind:key="p.nazwa">
+        <td>{{ i + 1 }}</td>
+        <td>{{ key }}</td>
+        <td>{{ p.nazwa }}</td>
+        <td>{{ p.cena }}</td>
+      </tr>
+      </tbody>
+    </table>
+    <div class="text-center">
+      <button v-for="i in 5" v-on:click="przechwycKllikniecieTabela" class="btn btn-primary">
+        Kliknij mnie
+      </button>
     </div>
-    <div class="bg-primary text-white text-center m-2 p-3">
-      <h3 v-if="licznik % 3 == 0">Produkt: {{nazwa}}</h3>
-      <h3 v-else-if="licznik % 3 == 1">Cena: {{cena}}</h3>
-      <h3 v-else>Kategoria: {{kategoria}}</h3>
-    </div>
-    <button class="btn btn-primary" v-on:click="przechwycKllikniecie">
-      Kliknij mnie
-    </button>
-    <button class="btn btn-primary" v-on:click="przechwycKllikniecieShow">
-      Albo kliknij mnie
-    </button>
   </div>
 </template>
 
@@ -31,37 +25,26 @@ export default {
   name: 'MojKomponent',
   data: function() {
     return {
-      nazwa: "Laptop",
-      cena: 1490,
-      nizszyPodatek: 12,
-      wyzszyPodatek: 23,
-      pokazElementy: true,
-      licznik: 0,
+      produktyObiekt: {
+        "kajak": { nazwa: "Kajak", cena: 234 },
+        22: { nazwa: "Kamizelka ratunkowa", cena: 49.99 },
+        3: { nazwa: "Piłka do metalu", cena: 19.49 },
+        "4": { nazwa: "Stadion", cena: 9990000 },
+        5: { nazwa: "Czapeczka", cena: 15 },
+      },
     }
   },
+  computed: {
+
+  },
   methods: {
-    przechwycKllikniecie() {
-      this.pokazElementy = !this.pokazElementy;
+    przechwycKllikniecieTabela() {
+      //this.produkty.push(this.produkty.shift())
+      //this.produkty = this.produkty.filter(p => p.cena > 20);
+      //this.produkty[1] = {nazwa: "Buty do biegania", cena: 99};
+      //this.produkty.length = 2;
+      Object.assign(this.produktyObiekt, {6: {nazwa: "Buty do biegania", cena: 99}});
     },
-    przechwycKllikniecieShow() {
-      this.licznik++;
-    },
-    pobierzCalkowitaCene(podatek) {
-      return this.cena + (this.cena * (podatek / 100))
-    },
-    waluta(wartosc, miejsc) {
-      return new Intl.NumberFormat("pl-PL",
-          {style: "currency", currency: "PLN",
-          minimumFractionDigits: miejsc || 2,
-          maximumFractionDigits: miejsc || 2
-          }).format(wartosc);
-    },
-    odwroc(wartosc) {
-      return wartosc.split("").reverse().join("");
-    },
-    wielka(wartosc){
-      return wartosc[0].toUpperCase() + wartosc.slice(1);
-    }
   },
 }
 </script>
