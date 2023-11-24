2398453e671b219601768ad4f83d9e3d8a8e509f
patrykcyran <83006094+patrykcyran@users.noreply.github.com>
2023-11-24 09:53:04 +0100

    mid

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="bg-primary text-white text-center m-2 p-3">
-    <h3>Produkt: {{ wielka(odwroc(nazwa))}}</h3>
-    <h3>Cena: {{ waluta(pobierzCalkowitaCene(nizszyPodatek), 3) }} PLN (niższa stawka)</h3>
-    <h3>Cena: {{ $filtry.walutaG(pobierzCalkowitaCene(wyzszyPodatek)) }} PLN (wyższa stawka)</h3>
+  <div class="container-fluid text-center">
+    <div class="bg-primary text-white text-center m-2 p-3">
+      <ul class="text-left">
+        <li>Pierwszy element listy</li>
+        <template v-if="pokazElementy">
+          <li>{{nazwa}}</li>
+          <li>{{cena}}</li>
+        </template>
+        <li>Ostatni element listy</li>
+      </ul>
+      <h3 v-if="pokazElementy">Produkt: {{nazwa}}</h3>
+      <h3 v-else>Cena: {{cena}}</h3>
+    </div>
+    <div class="bg-primary text-white text-center m-2 p-3">
+      <h3 v-if="licznik % 3 == 0">Produkt: {{nazwa}}</h3>
+      <h3 v-else-if="licznik % 3 == 1">Cena: {{cena}}</h3>
+      <h3 v-else>Kategoria: {{kategoria}}</h3>
+    </div>
+    <button class="btn btn-primary" v-on:click="przechwycKllikniecie">
+      Kliknij mnie
+    </button>
+    <button class="btn btn-primary" v-on:click="przechwycKllikniecieShow">
+      Albo kliknij mnie
+    </button>
   </div>
 </template>
 
@@ -14,10 +34,18 @@ export default {
       nazwa: "Laptop",
       cena: 1490,
       nizszyPodatek: 12,
-      wyzszyPodatek: 23
+      wyzszyPodatek: 23,
+      pokazElementy: true,
+      licznik: 0,
     }
   },
   methods: {
+    przechwycKllikniecie() {
+      this.pokazElementy = !this.pokazElementy;
+    },
+    przechwycKllikniecieShow() {
+      this.licznik++;
+    },
     pobierzCalkowitaCene(podatek) {
       return this.cena + (this.cena * (podatek / 100))
     },
